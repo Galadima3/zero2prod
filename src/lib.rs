b0cca5cb8ec267d2a7cc24c349bb75776f8334e1
Galadima3 <abrahamgaladima300@gmail.com>
2026-01-15 08:44:04 +0100

    CHORES: Minor fixes

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+// Run Function
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
         .listen(listener)?
